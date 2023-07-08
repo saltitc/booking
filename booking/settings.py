cabaaa4490637f4efe7953547acc56bfa5897d03
Ayvaz Izmailov <saltinthecut@gmail.com>
2023-07-08 14:11:30 +0300

    added appointments in user profile

--- booking/settings.py
+++ booking/settings.py
@@ -82,7 +82,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'studio.context_processors.get_notification'
+                'studio.context_processors.get_notification',
+                'studio.context_processors.get_user_appointments'
             ],
         },
     },
--- studio/context_processors.py
+++ studio/context_processors.py
@@ -7,3 +7,8 @@ def get_notification(request):
         'count_of_not_accepted_appointments': count
     }
     return data
+
+
+def get_user_appointments(request):
+    user = request.user
+    return {'user_appointments': Appointment.objects.filter(user=user.id) if user.is_authenticated else []}
